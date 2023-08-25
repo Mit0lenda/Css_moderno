6ef0af0c762e4aca8d88f12aee350bdc051afbbe
Nicollas De Oliveira <113867356+Mit0lenda@users.noreply.github.com>
2023-08-25 00:59:56 -0300

    p

---
+++ FlexBox/Exercicio_de_fixação 2/teste.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class teste {
+    public static void main(String[] args) throws Exception {
+        Date data = new Date();
+        System.out.print("Time:");
+        System.out.print(data.toString());
+    }
+    
+}
